8383bee2adf1e0eac20ce9209d378764d9d4a5af
irfan-002 <190497389+irfanhabeeb-002@users.noreply.github.com>
2025-09-11 10:20:26 +0530

    Fix GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   plugins: [
     react(),
     tailwindcss(),
